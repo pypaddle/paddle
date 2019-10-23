d769b89637d8a01ab40f32c304c88b1786c54877
Julian Stier <mail@julian-stier.de>
2019-10-23 17:20:10 +0200

    Intermediate implementation for MaskedDeepDAN. Still missing: enforcing masked structure

--- paddle/sparse.py
+++ paddle/sparse.py
@@ -6,20 +6,234 @@ import paddle.util
 from torch.autograd import Variable
 
 
+class LayeredGraph(nx.DiGraph):
+    @property
+    def num_layers(self):
+        """
+        :rtype: int
+        """
+        return NotImplementedError()
+
+    @property
+    def first_layer_size(self):
+        """
+        :rtype: int
+        """
+        return NotImplementedError()
+
+    @property
+    def last_layer_size(self):
+        """
+        :rtype: int
+        """
+        return NotImplementedError()
+
+    @property
+    def layers(self):
+        """
+        :rtype: list[int]
+        """
+        raise NotImplementedError()
+
+    def get_layer(self, vertex: int):
+        """
+        :rtype: int
+        """
+        raise NotImplementedError()
+
+    def get_vertices(self, layer: int):
+        """
+        :rtype: list[int]
+        """
+        raise NotImplementedError()
+
+    def get_layer_size(self, layer: int):
+        """
+        :rtype: int
+        """
+        raise NotImplementedError()
+
+    def layer_connected(self, layer_index1: int, layer_index2: int):
+        """
+        :rtype: bool
+        """
+        raise NotImplementedError()
+
+    def layer_connection_size(self, layer_index1: int, layer_index2: int):
+        """
+        :rtype: int
+        """
+        raise NotImplementedError()
+
+
+class CachedLayeredGraph(LayeredGraph):
+    def __init__(self, **attr):
+        super().__init__(**attr)
+        self._has_changed = True
+        self._layer_index = None
+        self._vertex_by_layer = None
+
+    def add_cycle(self, nodes, **attr):
+        super(LayeredGraph, self).add_cycle(nodes, **attr)
+        self._has_changed = True
+
+    def add_edge(self, u_of_edge, v_of_edge, **attr):
+        super(LayeredGraph, self).add_edge(u_of_edge, v_of_edge, **attr)
+        self._has_changed = True
+
+    def add_edges_from(self, ebunch_to_add, **attr):
+        super(LayeredGraph, self).add_edges_from(ebunch_to_add, **attr)
+        self._has_changed = True
+
+    def add_node(self, node_for_adding, **attr):
+        super(LayeredGraph, self).add_node(node_for_adding, **attr)
+        self._has_changed = True
+
+    def add_nodes_from(self, nodes_for_adding, **attr):
+        super(LayeredGraph, self).add_nodes_from(nodes_for_adding, **attr)
+        self._has_changed = True
+
+    def add_path(self, nodes, **attr):
+        super(LayeredGraph, self).add_path(nodes, **attr)
+        self._has_changed = True
+
+    def add_star(self, nodes, **attr):
+        super(LayeredGraph, self).add_star(nodes, **attr)
+        self._has_changed = True
+
+    def add_weighted_edges_from(self, ebunch_to_add, weight='weight', **attr):
+        super(LayeredGraph, self).add_weighted_edges_from(ebunch_to_add, weight='weight', **attr)
+        self._has_changed = True
+
+    def _get_layer_index(self):
+        if self._has_changed or self._layer_index is None or self._vertex_by_layer is None:
+            self._build_layer_index()
+            self._has_changed = False
+
+        return self._layer_index, self._vertex_by_layer
+
+    def _layer_by_vertex(self, vertex: int):
+        return self._get_layer_index()[0][vertex]
+
+    def _vertices_by_layer(self, layer: int):
+        return self._get_layer_index()[1][layer]
+
+    def _build_layer_index(self):
+        self._layer_index, self._vertex_by_layer = paddle.util.build_layer_index(self)
+
+    @property
+    def num_layers(self):
+        return len(self._get_layer_index()[0].keys())
+
+    @property
+    def first_layer_size(self):
+        return self.get_layer_size(self.layers[0])
+
+    @property
+    def last_layer_size(self):
+        return self.get_layer_size(self.layers[-1])
+
+    @property
+    def layers(self):
+        return [layer for layer in self._get_layer_index()[1]]
+
+    def get_layer(self, vertex: int):
+        return self._layer_by_vertex(vertex)
+
+    def get_vertices(self, layer: int):
+        return self._vertices_by_layer(layer)
+
+    def get_layer_size(self, layer: int):
+        return len(self._vertices_by_layer(layer))
+
+    def layer_connected(self, layer_index1: int, layer_index2: int):
+        """
+        :rtype: bool
+        """
+        if layer_index1 is layer_index2:
+            raise ValueError('Same layer does not have interconnections, it would be split up.')
+        if layer_index1 > layer_index2:
+            tmp = layer_index2
+            layer_index2 = layer_index1
+            layer_index1 = tmp
+
+        for source_vertex in self.get_vertices(layer_index1):
+            for target_vertex in self.get_vertices(layer_index2):
+                if self.has_edge(source_vertex, target_vertex):
+                    return True
+        return False
+
+    def layer_connection_size(self, layer_index1: int, layer_index2: int):
+        """
+        :rtype: int
+        """
+        if layer_index1 is layer_index2:
+            raise ValueError('Same layer does not have interconnections, it would be split up.')
+        if layer_index1 > layer_index2:
+            tmp = layer_index2
+            layer_index2 = layer_index1
+            layer_index1 = tmp
+
+        size = 0
+        for source_vertex in self.get_vertices(layer_index1):
+            for target_vertex in self.get_vertices(layer_index2):
+                if self.has_edge(source_vertex, target_vertex):
+                    size += 1
+        return size
+
+
+
+
 class MaskedDeepDAN(nn.Module):
     """
     A deep directed acyclic network model which is capable of masked layers and masked skip-layer connections.
     """
-    def __init__(self, input_size, num_classes, structure : nx.DiGraph):
+    def __init__(self, input_size, num_classes, structure : LayeredGraph):
         super(MaskedDeepDAN, self).__init__()
 
-        layer_index, vertex_by_layer = paddle.util.build_layer_index(structure)
-        layers = [l for l in vertex_by_layer]
+        #layer_index, vertex_by_layer = paddle.util.build_layer_index(structure)
+        #layers = [l for l in vertex_by_layer]
 
-        self.layer_first = MaskedLinearLayer(input_size, len(vertex_by_layer[0]))
-        self.layers_main_hidden = nn.ModuleList([MaskedLinearLayer(len(vertex_by_layer[l-1]), len(vertex_by_layer[l])) for l in layers[1:]])
-        self.layer_out = MaskedLinearLayer((len(vertex_by_layer[layers[-1]]), num_classes))
+        self._structure = structure
+        assert structure.num_layers > 0
+
+        self.layer_first = MaskedLinearLayer(input_size, structure.first_layer_size)
+        self.layers_main_hidden = nn.ModuleList([MaskedLinearLayer(structure.get_layer_size(l-1), structure.get_layer_size(l)) for l in structure.layers[1:]])
+
+        skip_layers = []
+        self._skip_targets = {}
+        for target_layer in structure.layers[2:]:
+            target_size = structure.get_layer_size(target_layer)
+            for distant_source_layer in structure.layers[:target_layer-1]:
+                if structure.layer_connected(distant_source_layer, target_layer):
+                    if target_layer not in self._skip_targets:
+                        self._skip_targets[target_layer] = []
+                    #print('Layer %s is connected to %s' % (distant_source_layer, target_layer))
+                    skip_layer = MaskedLinearLayer(structure.get_layer_size(distant_source_layer), target_size)
+                    skip_layers.append(skip_layer)
+                    self._skip_targets[target_layer].append({'layer': skip_layer, 'source': distant_source_layer})
+        self.layers_skip_hidden = nn.ModuleList(skip_layers)
+
+        self.layer_out = MaskedLinearLayer(structure.last_layer_size, num_classes)
         self.activation = nn.ReLU()
+
+    def forward(self, x):
+        last_output = self.activation(self.layer_first(x))
+        layer_results = {}
+        for layer, layer_idx in zip(self.layers_main_hidden, self._structure.layers[1:]):
+            out = self.activation(layer(last_output))
+
+            if layer_idx in self._skip_targets:
+                for skip_target in self._skip_targets[layer_idx]:
+                    source_layer = skip_target['layer']
+                    source_idx = skip_target['source']
+
+                    out += self.activation(source_layer(layer_results[source_idx]))
+
+            layer_results[layer_idx] = out  # copy?
+            last_output = out
+
+        return self.layer_out(last_output)
 
 
 class MaskedDeepFFN(nn.Module):
--- test/test_MaskedDeepDAN.py
+++ test/test_MaskedDeepDAN.py
@@ -1,22 +1,113 @@
 import unittest
 import networkx as nx
-import paddle
+import numpy as np
 import paddle.util
+import paddle.sparse
+
+import torch
+import torch.utils
+import torch.nn as nn
+from paddle.learning import train, test
+from torch.utils.data import SubsetRandomSampler
+from torchvision import datasets, transforms
 
 
 class MaskedDeepDANTest(unittest.TestCase):
 
     def test_dev(self):
-        structure = nx.DiGraph()
-        structure.add_nodes_from([1, 2, 3, 4, 5])
-        structure.add_edge(1, 3)
-        structure.add_edge(1, 4)
-        structure.add_edge(1, 5)
-        structure.add_edge(2, 3)
-        structure.add_edge(2, 4)
-        structure.add_edge(3, 5)
-        structure.add_edge(4, 5)
+        structure = paddle.sparse.CachedLayeredGraph()
+        structure.add_nodes_from(np.arange(1, 7))
 
-        layer_index, vertex_by_layer = paddle.util.build_layer_index(structure)
-        for layer in vertex_by_layer:
-            print(layer)
+        block0_size = 50
+        block1_size = 50
+        block2_size = 30
+        block3_size = 30
+        block4_size = 30
+        block5_size = 20
+        block6_size = 20
+        block0 = np.arange(1, block0_size+1)
+        block1 = np.arange(block0_size+1, block0_size+block1_size+1)
+        block2 = np.arange(block0_size+block1_size+1, block0_size+block1_size+block2_size+1)
+        block3 = np.arange(block0_size+block1_size+block2_size+1, block0_size+block1_size+block2_size+block3_size+1)
+        block4 = np.arange(block0_size+block1_size+block2_size+block3_size+1, block0_size+block1_size+block2_size+block3_size+block4_size+1)
+        block5 = np.arange(block0_size+block1_size+block2_size+block3_size+block4_size+1, block0_size+block1_size+block2_size+block3_size+block4_size+block5_size+1)
+        block6 = np.arange(block0_size+block1_size+block2_size+block3_size+block4_size+block5_size+1, block0_size+block1_size+block2_size+block3_size+block4_size+block5_size+block6_size+1)
+
+        # First layer
+        for v in block0:
+            for t in block2:
+                structure.add_edge(v, t)
+        for v in block0:
+            for t in block3:
+                structure.add_edge(v, t)
+        for v in block1:
+            for t in block3:
+                structure.add_edge(v, t)
+        for v in block1:
+            for t in block4:
+                structure.add_edge(v, t)
+
+        # Second layer
+        for v in block2:
+            for t in block5:
+                structure.add_edge(v, t)
+        for v in block2:
+            for t in block6:
+                structure.add_edge(v, t)
+        for v in block3:
+            for t in block5:
+                structure.add_edge(v, t)
+        for v in block3:
+            for t in block6:
+                structure.add_edge(v, t)
+        for v in block4:
+            for t in block5:
+                structure.add_edge(v, t)
+        for v in block4:
+            for t in block6:
+                structure.add_edge(v, t)
+
+        model = paddle.sparse.MaskedDeepDAN(784, 10, structure)
+        print(model)
+
+
+        batch_size = 100
+        epochs = 100
+
+        """
+        MNIST
+        """
+        custom_transform = transforms.Compose([
+            transforms.ToTensor(),  # first, convert image to PyTorch tensor
+            transforms.Normalize((0.1307,), (0.3081,))  # normalize inputs
+        ])
+        # download and transform train dataset
+        mnist_train_loader = torch.utils.data.DataLoader(datasets.MNIST('/media/data/set/mnist/',
+                                                                  download=True,
+                                                                  train=True,
+                                                                  transform=custom_transform),
+                                                                   batch_size=batch_size,
+                                                                   shuffle=True)
+
+        # download and transform test dataset
+        mnist_test_loader = torch.utils.data.DataLoader(datasets.MNIST('/media/data/set/mnist/',
+                                                                 download=True,
+                                                                 train=False,
+                                                                 transform=custom_transform),
+                                                                  batch_size=batch_size,
+                                                                  shuffle=True)
+
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        # Assuming that we are on a CUDA machine, this should print a CUDA device:
+        print(device)
+        model.to(device)
+
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+        loss_func = nn.CrossEntropyLoss()
+
+        for epoch in range(epochs):
+            print('Epoch', epoch)
+            loss, perc = train(mnist_train_loader, model, optimizer, loss_func, device, percentage=True)
+            print('Train', loss, perc)
+
+            print('Test', test(mnist_test_loader, model, device))
